184faa4ea4e9119543d37b7b0ec24e004b8db948
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-08-29 17:15:03 +0100

    Create do_show function in console

--- console.py
+++ console.py
@@ -10,32 +10,76 @@ classes = [
     "BaseModel"
 ]
 
+jsonpath = "models/engine/instances.json"
+
+
+# Parser function to validate commands
+def parser(argv):
+    """Validate command line arguments
+
+    Returns:
+        list of command line arguments or None if invalid commands
+    """
+    if len(argv) == 0:
+        print("** class name missing **")
+        return
+    # Split argv into a list of strings
+    args = argv.split()
+    # Check if class exists
+    if args[0] not in classes:
+        print("** class doesn't exist **")
+        return
+    return args
+
 
 class HBNBCommand(cmd.Cmd):
     """Contains functions for command interpreter"""
-
-
     prompt = "(hbnb) "
 
     def do_create(self, argv):
-        """Creates a new instance of a class, saves it to JSON file and prints ID"""
+        """
+        Creates a new instance of a class,
+        saves it to JSON file and prints ID
+        """
 
-        if len(argv) == 0:
-            print("** class name missing **")
+        args = parser(argv)
+        if args is None:
             return
-        # Split argv into a list of strings
-        args = argv.split()
-        # Check if class exists
-        if args[0] not in classes:
-            print("** class doesn't exist **")
-        else:
-            instance = BaseModel()
-            # save to json file
-            with open("models/engine/instances.json", mode="r+", encoding="utf-8") as f:
-                if len(f.read()) != 0:
-                    f.write("\n")
-                f.write(json.dumps(instance.to_dict()))
-            print(instance.id)
+        instance = BaseModel()
+        # save to json file
+        with open(jsonpath, mode="r+", encoding="utf-8") as f:
+            if len(f.read()) != 0:
+                f.write("\n")
+            f.write(json.dumps(instance.to_dict()))
+        print(instance.id)
+
+    def do_show(self, argv):
+        """
+        Prints the sting representation of an instance based on the
+        class name and id
+        """
+
+        args = parser(argv)
+        if args is None:
+            return
+        if len(args) == 1:  # id is missing
+            print("** instance id missing **")
+            return
+        # Load json file and populate instances
+        with open(jsonpath, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+            instances = []
+            for line in lines:
+                instances.append(json.loads(line))
+
+        # Search instances
+        for instance in instances:
+            if instance["id"] == args[1]:
+                # create instance
+                inst = BaseModel(**instance)
+                print(inst)
+                return
+        print("** no instance found **")
 
     def do_quit(self, argv):
         """Quits command interpreter"""
@@ -47,7 +91,6 @@ class HBNBCommand(cmd.Cmd):
 
         print()
         return True
-
 
 
 if __name__ == '__main__':

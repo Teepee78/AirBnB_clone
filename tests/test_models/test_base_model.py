80053bb00b40bc8a0964add3b4dfaa47dda5aca5
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-08-29 13:48:41 +0100

    Update init method to make an instance from keyword arguments

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -21,6 +21,26 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(inst2.created_at.strftime("%H %M %S"), time)
         self.assertEqual(inst2.updated_at.strftime("%H %M %S"), time)
 
+    def test_init_kwargs(self):
+        """Test initialization with keyword arguments"""
+
+        dictionary = {
+            "id": "56d43177-cc5f-4d6c-a0c1-e167f8c27337",
+            "created_at": "2017-09-28T21:03:54.052298",
+            "__class__": "BaseModel",
+            "my_number": 89,
+            "updated_at": "2017-09-28T21:03:54.052302",
+            "name": "My_First_Model"
+        }
+        inst = BaseModel(**dictionary)
+        self.assertEqual(inst.id, "56d43177-cc5f-4d6c-a0c1-e167f8c27337")
+        created_at = datetime(2017, 9, 28, 21, 3, 54, 52298)
+        self.assertEqual(inst.created_at, created_at)
+        self.assertEqual(inst.my_number, 89)
+        updated_at = datetime(2017, 9, 28, 21, 3, 54, 52302)
+        self.assertEqual(inst.updated_at, updated_at)
+        self.assertEqual(inst.name, "My_First_Model")
+
     def test_init_args(self):
         """Test initialization with parameters"""
 

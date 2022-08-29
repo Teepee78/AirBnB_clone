10d1463b471089910551c001a7982d64e7779595
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-08-29 16:06:57 +0100

    Create 'do_create' function in console.py

--- console.py
+++ console.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 """Entry point of the command interpreter"""
 import cmd
+import json
+from models.base_model import BaseModel
+
+
+# List of classes
+classes = [
+    "BaseModel"
+]
 
 
 class HBNBCommand(cmd.Cmd):
@@ -8,6 +16,26 @@ class HBNBCommand(cmd.Cmd):
 
 
     prompt = "(hbnb) "
+
+    def do_create(self, argv):
+        """Creates a new instance of a class, saves it to JSON file and prints ID"""
+
+        if len(argv) == 0:
+            print("** class name missing **")
+            return
+        # Split argv into a list of strings
+        args = argv.split()
+        # Check if class exists
+        if args[0] not in classes:
+            print("** class doesn't exist **")
+        else:
+            instance = BaseModel()
+            # save to json file
+            with open("models/engine/instances.json", mode="r+", encoding="utf-8") as f:
+                if len(f.read()) != 0:
+                    f.write("\n")
+                f.write(json.dumps(instance.to_dict()))
+            print(instance.id)
 
     def do_quit(self, argv):
         """Quits command interpreter"""
